69ecba2ad46d4214086c80f5d9619104575446ad
IhorC <igorchua@gmail.com>
2023-03-09 23:42:38 +0200

    main tasks finished

--- src/handlebars/images.precompiled.js
+++ src/handlebars/images.precompiled.js
@@ -8,23 +8,19 @@ templates['images'] = template({"1":function(container,depth0,helpers,partials,d
         return undefined
     };
 
-  return "        <li class=\"container__item\">\r\n            <a class=\"container__link\" href="
-    + alias4(((helper = (helper = lookupProperty(helpers,"largeImageURL") || (depth0 != null ? lookupProperty(depth0,"largeImageURL") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"largeImageURL","hash":{},"data":data,"loc":{"start":{"line":4,"column":44},"end":{"line":4,"column":61}}}) : helper)))
-    + ">\r\n                <img \r\n                    src="
-    + alias4(((helper = (helper = lookupProperty(helpers,"previewURL") || (depth0 != null ? lookupProperty(depth0,"previewURL") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"previewURL","hash":{},"data":data,"loc":{"start":{"line":6,"column":24},"end":{"line":6,"column":38}}}) : helper)))
-    + " \r\n                    alt="
-    + alias4(((helper = (helper = lookupProperty(helpers,"tags") || (depth0 != null ? lookupProperty(depth0,"tags") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"tags","hash":{},"data":data,"loc":{"start":{"line":7,"column":24},"end":{"line":7,"column":32}}}) : helper)))
-    + " \r\n                    class=\"container__img\"\r\n                    data-source="
-    + alias4(((helper = (helper = lookupProperty(helpers,"largeImageURL") || (depth0 != null ? lookupProperty(depth0,"largeImageURL") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"largeImageURL","hash":{},"data":data,"loc":{"start":{"line":9,"column":32},"end":{"line":9,"column":49}}}) : helper)))
-    + "\r\n                >\r\n            </a>\r\n            <ul class=\"container__descr-list\">\r\n                <li class=\"container__descr-item\">\r\n                    <p>\r\n                        <span class=\"container__descr-item--span\">Likes: </span>"
-    + alias4(((helper = (helper = lookupProperty(helpers,"likes") || (depth0 != null ? lookupProperty(depth0,"likes") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"likes","hash":{},"data":data,"loc":{"start":{"line":15,"column":80},"end":{"line":15,"column":89}}}) : helper)))
-    + "\r\n                    </p>\r\n                </li>\r\n                <li class=\"container__descr-item\">\r\n                    <p>\r\n                        <span class=\"container__descr-item--span\">Views: </span>"
-    + alias4(((helper = (helper = lookupProperty(helpers,"views") || (depth0 != null ? lookupProperty(depth0,"views") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"views","hash":{},"data":data,"loc":{"start":{"line":20,"column":80},"end":{"line":20,"column":89}}}) : helper)))
-    + "\r\n                    </p>\r\n                </li>\r\n                <li class=\"container__descr-item\">\r\n                    <p>\r\n                        <span class=\"container__descr-item--span\">Comments: </span>"
-    + alias4(((helper = (helper = lookupProperty(helpers,"comments") || (depth0 != null ? lookupProperty(depth0,"comments") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"comments","hash":{},"data":data,"loc":{"start":{"line":25,"column":83},"end":{"line":25,"column":95}}}) : helper)))
-    + "\r\n                    </p>\r\n                </li>\r\n                <li class=\"container__descr-item\">\r\n                    <p>\r\n                        <span class=\"container__descr-item--span\">Downloads: </span>"
-    + alias4(((helper = (helper = lookupProperty(helpers,"downloads") || (depth0 != null ? lookupProperty(depth0,"downloads") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"downloads","hash":{},"data":data,"loc":{"start":{"line":30,"column":84},"end":{"line":30,"column":97}}}) : helper)))
-    + "\r\n                    </p>\r\n                </li>\r\n            </ul>\r\n        </li>\r\n";
+  return "        <div class=\"photo-card\">\r\n            <a href="
+    + alias4(((helper = (helper = lookupProperty(helpers,"largeImageURL") || (depth0 != null ? lookupProperty(depth0,"largeImageURL") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"largeImageURL","hash":{},"data":data,"loc":{"start":{"line":3,"column":20},"end":{"line":3,"column":37}}}) : helper)))
+    + ">\r\n                <img src="
+    + alias4(((helper = (helper = lookupProperty(helpers,"webformatURL") || (depth0 != null ? lookupProperty(depth0,"webformatURL") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"webformatURL","hash":{},"data":data,"loc":{"start":{"line":4,"column":25},"end":{"line":4,"column":41}}}) : helper)))
+    + " alt=\"\" loading=\"lazy\" />\r\n            </a>\r\n            <div class=\"info\">\r\n                <p class=\"info-item\">\r\n                    <b>Likes</b>\r\n                    "
+    + alias4(((helper = (helper = lookupProperty(helpers,"likes") || (depth0 != null ? lookupProperty(depth0,"likes") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"likes","hash":{},"data":data,"loc":{"start":{"line":9,"column":20},"end":{"line":9,"column":29}}}) : helper)))
+    + "\r\n                </p>\r\n                <p class=\"info-item\">\r\n                    <b>Views</b>\r\n                    "
+    + alias4(((helper = (helper = lookupProperty(helpers,"views") || (depth0 != null ? lookupProperty(depth0,"views") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"views","hash":{},"data":data,"loc":{"start":{"line":13,"column":20},"end":{"line":13,"column":29}}}) : helper)))
+    + "\r\n                </p>\r\n                <p class=\"info-item\">\r\n                    <b>Comments</b>\r\n                    "
+    + alias4(((helper = (helper = lookupProperty(helpers,"comments") || (depth0 != null ? lookupProperty(depth0,"comments") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"comments","hash":{},"data":data,"loc":{"start":{"line":17,"column":20},"end":{"line":17,"column":32}}}) : helper)))
+    + "\r\n                </p>\r\n                <p class=\"info-item\">\r\n                    <b>Downloads</b>\r\n                    "
+    + alias4(((helper = (helper = lookupProperty(helpers,"downloads") || (depth0 != null ? lookupProperty(depth0,"downloads") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"downloads","hash":{},"data":data,"loc":{"start":{"line":21,"column":20},"end":{"line":21,"column":33}}}) : helper)))
+    + "\r\n                </p>\r\n            </div>  \r\n        </div>\r\n";
 },"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1, lookupProperty = container.lookupProperty || function(parent, propertyName) {
         if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
@@ -33,8 +29,7 @@ templates['images'] = template({"1":function(container,depth0,helpers,partials,d
         return undefined
     };
 
-  return "<ul class=\"container__img-list\">\r\n"
-    + ((stack1 = lookupProperty(helpers,"each").call(depth0 != null ? depth0 : (container.nullContext || {}),(depth0 != null ? lookupProperty(depth0,"arr") : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":2,"column":4},"end":{"line":35,"column":13}}})) != null ? stack1 : "")
-    + "</ul>\r\n\r\n";
+  return ((stack1 = lookupProperty(helpers,"each").call(depth0 != null ? depth0 : (container.nullContext || {}),(depth0 != null ? lookupProperty(depth0,"arr") : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":1,"column":4},"end":{"line":25,"column":13}}})) != null ? stack1 : "")
+    + "\r\n";
 },"useData":true});
 })();
--- src/index.js
+++ src/index.js
@@ -1,39 +1,76 @@
 import Notiflix from 'notiflix';
-//const Handlebars = require("handlebars");
-// console.log(Handlebars.template)
-console.log(window.Handlebars)
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-
-// const template = Handlebars.compile("Name: {{name}}");
-// console.log(template({ name: "Nils" }));
 const API_KEY = "34265158-b3e7c04db650eceaa44e6318e";
-const IMG_ADRESS = "https://pixabay.com/api/";
 const form = document.querySelector('.search-form');
-const formBtn = document.querySelector('[type="submit"]');
-const imgDiv = document.querySelector('.images');
+const loadMoreBtn = document.querySelector('.loadMoreBtn');
+const gallery = document.querySelector('.gallery');
 const template = Handlebars.templates;
+const hiddennBtn = document.querySelector('.hidden');
 
 let page = 1;
-let limit = 5;
+let limit = 40;
+let category = '';
+let totalHits = 0;
 
 form.addEventListener('submit', formHandler);
+loadMoreBtn.addEventListener('click', loadMore);
  
+const lightbox = new SimpleLightbox('.gallery .photo-card', { 
+    captionSelector: 'img',
+    captionDelay: 250
+})
+
 function formHandler(event){
     event.preventDefault();
     const inputVal = event.target.elements.searchQuery.value;
-    
+    category = inputVal;
+    page = 1;
+
+    hiddennBtn.style.visibility = 'hidden';
+
     fetchImages(inputVal).then((images)=>{
         const arr = images.hits;
-        console.log('formHandaler result: ', arr)
-        imgDiv.innerHTML = template.images({arr})
+        totalHits = images.totalHits;
+        
+        if(arr.length === 0){
+            Notiflix.Notify.info("Sorry, there are no images matching your search query. Please try again.")
+            return
+        }
+        
+        gallery.innerHTML = template.images({arr});
+        hiddennBtn.style.visibility = 'visible';
     })
     .catch(error=>console.log(error.message));
+
+    lightbox.refresh();
 }
 
-async function fetchImages(val){
+function loadMore(){
+    page += 1;
+    // console.log(totalHits)
+    if(totalHits === 0){
+        Notiflix.Notify.info("We're sorry, but you've reached the end of search results.")
+        hiddennBtn.style.visibility = 'hidden';
+        return;
+    }
+
+    fetchImages(category).then((images)=>{
+        const arr = images.hits;
+        totalHits -= arr.length;
+        // console.log('loadMore result: ', arr)
+        gallery.insertAdjacentHTML('beforeend', template.images({arr}));
+    })
+    .catch(error=>console.log(error.message));
+    
+    lightbox.refresh();
+}
+
+async function fetchImages(){
     const searchParams = new URLSearchParams({
         key: API_KEY,
-        q: val,
+        q: category,
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: 'true',
@@ -49,9 +86,18 @@ async function fetchImages(val){
     } catch (error){
         console.log(error.message);
     }
-    // console.log(event.target.elements.searchQuery.value)
 }
 
+
+// let gallery = new SimpleLightbox('.container__img-list a', {
+//     captionSelector: 'img',
+//     captionDelay: 250
+// });
+
+// let gallery = new SimpleLightbox('.gallery a');
+// gallery.on('show.simplelightbox', function () {
+// 	// do something…
+// });
 // webformatURL - посилання на маленьке зображення для списку карток.
 // largeImageURL - посилання на велике зображення.
 // tags - рядок з описом зображення. Підійде для атрибуту alt.
