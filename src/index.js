8ed78b5660e6315428a6cc3768ae9f3d7b6c44f0
IhorC <igorchua@gmail.com>
2023-03-10 13:25:13 +0200

    bug fixes

--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ const hiddenBtn = document.querySelector('.hidden');
 let page = 1;
 let limit = 40;
 let category = '';
-let totalHits = 0;
 
 form.addEventListener('submit', formHandler);
 loadMoreBtn.addEventListener('click', loadMore);
@@ -22,77 +21,38 @@ loadMoreBtn.addEventListener('click', loadMore);
 // const lightbox = new SimpleLightbox('.gallery .photo-card', { 
 //     captionSelector: 'img',
 //     captionDelay: 250
-// })
+//})
 
 async function formHandler(event){
     event.preventDefault();
     const inputVal = event.target.elements.searchQuery.value.trim();
     let arr;
-    let data;
-    
+   
     hiddenBtn.style.visibility = 'hidden';
     category = inputVal;
+    page = 1;
     
     try {
         const fetchedData = await fetchImages(inputVal);
         arr = fetchedData.data.hits;
-        data = fetchedData;
-        console.log('formHandler() ', fetchedData);
+        
+        if(fetchedData.data.hits.length === 0){
+            Notiflix.Notify.info("Sorry, there are no images matching your search query. Please try again.")
+            gallery.innerHTML = '';
+            return
+        }
+        // console.log('formHandler() ', fetchedData);
     } catch(error){
         console.log(error.message);
     }
     
-    if(arr.length === 0){
-        Notiflix.Notify.info("Sorry, there are no images matching your search query. Please try again.")
-        return
-    }
-    
-    totalHits = data.totalHits;
-    page = 1;
-
     gallery.innerHTML = template.images({arr});
     hiddenBtn.style.visibility = 'visible';
-    
-    // fetchImages(inputVal).then((images)=>{
-    //     const arr = images.hits;
-    //     totalHits = images.totalHits;
-    //     console.log('totalHits: ', totalHits)
-        
-    //     if(arr.length === 0){
-    //         Notiflix.Notify.info("Sorry, there are no images matching your search query. Please try again.")
-    //         return
-    //     }
-        
-    //     gallery.innerHTML = template.images({arr});
-    //     hiddenBtn.style.visibility = 'visible';
-    // })
-    // .catch(error=>console.log(error.message));
-
-    // lightbox.refresh();
 }
 
 function loadMore(){
     page += 1;
-    
-    if(totalHits === 0){
-        Notiflix.Notify.info("We're sorry, but you've reached the end of search results.")
-        hiddenBtn.style.visibility = 'hidden';
-        return;
-    }
-
     loadMoreFetch();
-
-    // fetchImages(category).then((images)=>{
-    //     const arr = images.hits;
-    //     totalHits -= arr.length;
-    //     console.log(totalHits)
-
-    //     // console.log('loadMore result: ', arr)
-    //     gallery.insertAdjacentHTML('beforeend', template.images({arr}));
-    // })
-    // .catch(error=>console.log(error.message));
-    
-    // lightbox.refresh();
 }
 
 async function loadMoreFetch(){
@@ -100,10 +60,13 @@ async function loadMoreFetch(){
     try {
         const fetch = await fetchImages(category);
         arr = fetch.data.hits;
-        totalHits -= arr.length;
-
     } catch (error){
-        console.log(error.message);
+        if(error.code === "ERR_BAD_REQUEST"){
+            hiddenBtn.style.visibility = 'hidden';
+            Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+            return;
+        }
+        console.log(error);
     }
     
     gallery.insertAdjacentHTML('beforeend', template.images({arr}));
@@ -121,7 +84,7 @@ async function fetchImages(){
     });
 
     const response = await axios.get(`https://pixabay.com/api/?${searchParams}`);
-    console.log('fetchImages() result: ', response);
+    // console.log('fetchImages() result: ', response);
     return response;
 }
 
@@ -135,10 +98,3 @@ async function fetchImages(){
 // gallery.on('show.simplelightbox', function () {
 // 	// do something…
 // });
-// webformatURL - посилання на маленьке зображення для списку карток.
-// largeImageURL - посилання на велике зображення.
-// tags - рядок з описом зображення. Підійде для атрибуту alt.
-// likes - кількість лайків.
-// views - кількість переглядів.
-// comments - кількість коментарів.
-// downloads - кількість завантажень.
